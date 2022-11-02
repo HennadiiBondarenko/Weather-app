e34b92d041737371b83cbfc829b1166e579edfdc
Gena <hennadiibondarenko@gmail.com>
2022-11-01 19:30:12 -0600

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4f3a3b35dbmshf3288672bcfe2b6p171cf0jsn26e31216f40c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "dd7515d0c978f302cf0bebc8f144c10b";
